5f943cba307048caba06799b6ab4aadcda71e0e8
Emre Avara <emreavara@Emres-MacBook-Pro.local>
2024-03-13 00:01:37 +0100

    Fix 1.2

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,6 @@ import "dotenv/config";
 import app from "./index.js";
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
-
 app.listen(port, () => {
   console.log(`the server listening on ${port}`);
 });
