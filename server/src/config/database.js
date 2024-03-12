5b337e5374926ae7cc5a63547d62ef0c46b2fa57
Emre Avara <emreavara@Emres-MacBook-Pro.local>
2024-03-13 00:05:58 +0100

    Fix 1.3

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,38 +1,35 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
-const sequelize = new Sequelize(
-    'employee',
-    'employee',
-    'admin',
-    {
-        host: 'localhost',
-        port: 23308,
-        dialect: 'mysql',
-        logging: false,
-        define: {
-            underscored: true,
-            freezeTableName: true,
-        },
-    });
+const sequelize = new Sequelize("employee", "employee", "admin", {
+  host: "localhost",
+  port: 23308,
+  dialect: "mysql",
+  dialectModule: mysql2,
+  logging: false,
+  define: {
+    underscored: true,
+    freezeTableName: true,
+  },
+});
 
 const connectToDatabase = async () => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync({ force: false, alter: true });
-    } catch (error) {
-        console.error("Unable to connect to the database: ", error);
-    }
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ force: false, alter: true });
+  } catch (error) {
+    console.error("Unable to connect to the database: ", error);
+  }
 };
 
 connectToDatabase();
-
 
 export default sequelize;
 
 // import * as dotenv from "dotenv";
 // import { Sequelize } from "sequelize";
 // dotenv.config();
-  
+
 // const options = {
 //     host: process.env.DB_HOST,
 //     port: process.env.DB_PORT,
@@ -43,7 +40,7 @@ export default sequelize;
 //   // if(options.dialect ==="mysql"){
 //   //   options.dialect = mysql2;
 //   // }
-  
+
 //   const sequelize = new Sequelize(
 //     process.env.DB_NAME,
 //     process.env.DB_USERNAME,
