be22c67ef6b301ecb755a6792e3cea074ee1a86e
Emre Avara <emreavara@Emres-MacBook-Pro.local>
2024-03-12 23:21:12 +0100

    Fix 5

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(logHandler);
 
-app.use("/", (request, response) => {
+app.get("/", (request, response) => {
   response.send("Welcome");
 });
 
